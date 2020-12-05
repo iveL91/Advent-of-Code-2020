1691655ed82c15b5abe3ecfa11772ee65ca3b9c8
Dominik <Dominik.Schillo@googlemail.com>
2020-12-05 01:01:46 +0100

    Redesign Day04 (OOP)

--- 04/main.py
+++ 04/main.py
@@ -4,6 +4,116 @@
     Time: ca. (26 + 58)min
 """
 
+from abc import ABC, abstractmethod
+from typing import Optional
+
+
+def in_range(number: int, lower_bound: int, upper_bound: int) -> bool:
+    return number in range(lower_bound, upper_bound+1)
+
+
+class PassportInformation(ABC):
+    def __init__(self, value: Optional[str]) -> None:
+        self.prop = value
+
+    def exists(self) -> bool:
+        return self.prop
+
+    @abstractmethod
+    def __bool__(self) -> bool:
+        pass
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.prop})"
+
+
+class Year(PassportInformation):
+    def __bool__(self, lower_bound: int, upper_bound: int) -> bool:
+        if self.prop is None:
+            return False
+        return (len(self.prop) == 4) and in_range(int(self.prop), lower_bound, upper_bound)
+
+
+class BirthYear(Year):
+    def __bool__(self) -> bool:
+        return super().__bool__(1920, 2002)
+
+
+class IssueYear(Year):
+    def __bool__(self) -> bool:
+        return super().__bool__(2010, 2020)
+
+
+class ExpirationYear(Year):
+    def __bool__(self) -> bool:
+        return super().__bool__(2020, 2030)
+
+
+class Height(PassportInformation):
+    units: list[str] = ["cm", "in"]
+    ranges: dict[str, tuple[int, int]] = {
+        unit: bounds for unit, bounds in zip(units, [(150, 193), (59, 76)])}
+
+    def __bool__(self) -> bool:
+        if self.prop is None:
+            return False
+        if (unit := self.prop[-2:]) in ["cm", "in"]:
+            return in_range(int(self.prop[:-2]), *self.ranges[unit])
+        return False
+
+
+class HairColor(PassportInformation):
+    def __bool__(self) -> bool:
+        if self.prop is None:
+            return False
+        if self.prop[0] != "#":
+            return False
+        if len(self.prop) != 7:
+            return False
+        valid_characters = list(str(1234567890)) + list("abcdefgh")
+        return all(character in valid_characters for character in self.prop[1:])
+
+
+class EyeColor(PassportInformation):
+    def __bool__(self) -> bool:
+        if self.prop is None:
+            return False
+        return self.prop in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+
+
+class PassportID(PassportInformation):
+    def __bool__(self) -> bool:
+        if self.prop is None or len(self.prop) != 9:
+            return False
+        numbers = list(str(1234567890))
+        return all(character in numbers for character in self.prop)
+
+
+class CountryID(PassportInformation):
+    def __bool__(self) -> bool:
+        return True
+
+    def exists(self) -> bool:
+        return True
+
+
+class Passport:
+    def __init__(self, dct: dict[str, str]) -> None:
+        self.byr = BirthYear(dct.get("byr", None))
+        self.iyr = IssueYear(dct.get("iyr", None))
+        self.eyr = ExpirationYear(dct.get("eyr", None))
+        self.hgt = Height(dct.get("hgt", None))
+        self.hcl = HairColor(dct.get("hcl", None))
+        self.ecl = EyeColor(dct.get("ecl", None))
+        self.pid = PassportID(dct.get("pid", None))
+        self.cid = CountryID(dct.get("cid", None))
+
+    def exists(self) -> bool:
+        return all(var.exists() for var in vars(self).values())
+
+    def __bool__(self) -> bool:
+        return all(var for var in vars(self).values())
+
 
 def data_input(filename: str = "data") -> list[dict[str, str]]:
     with open(filename) as file:
@@ -12,93 +122,15 @@ def data_input(filename: str = "data") -> list[dict[str, str]]:
 
 def string_to_passport(string: str) -> dict[str, str]:
     passport_list = [property.split(":") for property in string.split()]
-    return {property[0]: property[1] for property in passport_list}
+    return Passport({property[0]: property[1] for property in passport_list})
 
 
-def valid_range(number: int, lower_bound: int, upper_bound: int) -> bool:
-    return lower_bound <= number <= upper_bound
+def part_1(passports) -> int:
+    return sum(passport.exists() for passport in passports)
 
 
-def valid_byr(byr: str) -> bool:
-    return (len(byr) == 4) and valid_range(int(byr), 1920, 2002)
-
-
-def valid_iyr(iyr: str) -> bool:
-    return (len(iyr) == 4) and valid_range(int(iyr), 2010, 2020)
-
-
-def valid_eyr(eyr: str) -> bool:
-    return (len(eyr) == 4) and valid_range(int(eyr), 2020, 2030)
-
-
-def valid_hgt(hgt: str) -> bool:
-    if hgt[-2:] == "cm":
-        return valid_range(int(hgt[:-2]), 150, 193)
-    if hgt[-2:] == "in":
-        return valid_range(int(hgt[:-2]), 59, 76)
-    return False
-
-
-def valid_hcl(hcl: str) -> bool:
-    if hcl[0] != "#":
-        return False
-    if len(hcl) != 7:
-        return False
-    valid_characters = list(str(1234567890)) + list("abcdefgh")
-    return all(character in valid_characters for character in hcl[1:])
-
-
-def valid_ecl(ecl: str) -> bool:
-    return ecl in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
-
-
-def valid_pid(pid: str) -> bool:
-    numbers = list(str(1234567890))
-    if len(pid) != 9:
-        return False
-    return all(character in numbers for character in pid)
-
-
-def valid_cid(cid: str) -> bool:
-    return True
-
-
-def valid_passport(passport) -> bool:
-    if len(passport) < 7:
-        return False
-    if len(passport) == 8:
-        return True
-    for key in passport:
-        if key == "cid":
-            return False
-    return True
-
-
-def part_1(passports):
-    return sum(valid_passport(passport) for passport in passports)
-
-
-def valid_new_passport(passport) -> bool:
-    validation_dict = {"byr": valid_byr,
-                       "iyr": valid_iyr,
-                       "eyr": valid_eyr,
-                       "hgt": valid_hgt,
-                       "hcl": valid_hcl,
-                       "ecl": valid_ecl,
-                       "pid": valid_pid,
-                       "cid": valid_cid}
-    if not valid_range(len(passport), 7, 8):
-        return False
-    for key, value in passport.items():
-        if not validation_dict[key](value):
-            return False
-        if len(passport) == 7 and key == "cid":
-            return False
-    return True
-
-
-def part_2(passports):
-    return sum(valid_new_passport(passport) for passport in passports)
+def part_2(passports) -> int:
+    return sum(bool(passport) for passport in passports)
 
 
 def main() -> None:
@@ -114,7 +146,7 @@ def main() -> None:
 if __name__ == "__main__":
     main()
 
-    import timeit
-    passports = data_input("data")
-    print(timeit.timeit("part_1(passports)", globals=globals(), number=10_000))
-    print(timeit.timeit("part_2(passports)", globals=globals(), number=10_000))
+    # import timeit
+    # passports = data_input("data")
+    # print(timeit.timeit("part_1(passports)", globals=globals(), number=10_000))
+    # print(timeit.timeit("part_2(passports)", globals=globals(), number=10_000))
--- 04/test.py
+++ 04/test.py
@@ -1,7 +1,7 @@
 """test_aoc_04"""
 
 import unittest
-from main import data_input, string_to_passport, valid_byr, valid_hgt, valid_hcl, valid_ecl, valid_pid, valid_new_passport, part_1
+from main import data_input, string_to_passport, Passport, BirthYear, Height, HairColor, EyeColor, PassportID, part_1
 
 
 class TestAoC04(unittest.TestCase):
@@ -14,88 +14,89 @@ class TestAoC04(unittest.TestCase):
         self.assertEqual(result, 2)
 
     def test_valid_byr_valid(self):
-        self.assertTrue(valid_byr("2002"))
+        self.assertTrue(BirthYear("2002"))
 
     def test_valid_byr_invalid(self):
-        self.assertFalse(valid_byr("2003"))
+        self.assertFalse(BirthYear("2003"))
 
     def test_valid_hgt_valid_in(self):
-        self.assertTrue(valid_hgt("60in"))
+        self.assertTrue(Height("60in"))
 
     def test_valid_hgt_valid_cm(self):
-        self.assertTrue(valid_hgt("190cm"))
+        self.assertTrue(Height("190cm"))
 
     def test_valid_hgt_invalid_in(self):
-        self.assertFalse(valid_hgt("190in"))
+        self.assertFalse(Height("190in"))
 
     def test_valid_hgt_invalid(self):
-        self.assertFalse(valid_hgt("190"))
+        self.assertFalse(Height("190"))
 
     def test_valid_hcl_valid(self):
-        self.assertTrue(valid_hcl("#123abc"))
+        self.assertTrue(HairColor("#123abc"))
 
     def test_valid_hcl_invalid(self):
-        self.assertFalse(valid_hcl("#123abz"))
+        self.assertFalse(HairColor("#123abz"))
 
     def test_valid_hcl_invalid_no_hashtag(self):
-        self.assertFalse(valid_hcl("123abz"))
+        self.assertFalse(HairColor("123abz"))
 
     def test_valid_ecl_valid(self):
-        self.assertTrue(valid_ecl("brn"))
+        self.assertTrue(EyeColor("brn"))
 
     def test_valid_ecl_invalid(self):
-        self.assertFalse(valid_ecl("war"))
+        self.assertFalse(EyeColor("war"))
 
     def test_valid_pid_valid(self):
-        self.assertTrue(valid_pid("000000001"))
+        self.assertTrue(PassportID("000000001"))
 
     def test_valid_pid_invalid(self):
-        self.assertFalse(valid_pid("0123456789"))
+        self.assertFalse(PassportID("0123456789"))
 
     def test_valid_new_passport_invalid_1(self):
         passport = string_to_passport(
             "eyr:1972 cid:100\nhcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926")
-        self.assertFalse(valid_new_passport(passport))
+        print(passport)
+        self.assertFalse(passport)
 
     def test_valid_new_passport_invalid_2(self):
         passport = string_to_passport("""iyr:2019
 hcl:#602927 eyr:1967 hgt:170cm
 ecl:grn pid:012533040 byr:1946""")
-        self.assertFalse(valid_new_passport(passport))
+        self.assertFalse(passport)
 
     def test_valid_new_passport_invalid_3(self):
         passport = string_to_passport("""hcl:dab227 iyr:2012
 ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
 """)
-        self.assertFalse(valid_new_passport(passport))
+        self.assertFalse(passport)
 
     def test_valid_new_passport_invalid_4(self):
         passport = string_to_passport("""hgt:59cm ecl:zzz
 eyr:2038 hcl:74454a iyr:2023
 pid:3556412378 byr:2007""")
-        self.assertFalse(valid_new_passport(passport))
+        self.assertFalse(passport)
 
     def test_valid_new_passport_valid_1(self):
         passport = string_to_passport("""pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
 hcl:#623a2f""")
-        self.assertTrue(valid_new_passport(passport))
+        self.assertTrue(passport)
 
     def test_valid_new_passport_valid_2(self):
         passport = string_to_passport("""eyr:2029 ecl:blu cid:129 byr:1989
 iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm""")
-        self.assertTrue(valid_new_passport(passport))
+        self.assertTrue(passport)
 
     def test_valid_new_passport_valid_3(self):
         passport = string_to_passport("""hcl:#888785
 hgt:164cm byr:2001 iyr:2015 cid:88
 pid:545766238 ecl:hzl
 eyr:2022""")
-        self.assertTrue(valid_new_passport(passport))
+        self.assertTrue(passport)
 
     def test_valid_new_passport_valid_4(self):
         passport = string_to_passport(
             """iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719""")
-        self.assertTrue(valid_new_passport(passport))
+        self.assertTrue(passport)
 
 
 if __name__ == "__main__":
