1ec3f9ad832f0955715f56cb5d5451fcfd79b265
Dominik <Dominik.Schillo@googlemail.com>
2020-12-07 12:29:18 +0100

    Day07 completed

--- 07/main.py
+++ 07/main.py
@@ -1,35 +1,96 @@
 """
     aoc_07
     https://adventofcode.com/2020/day/7
-    Time: ca. ( + )min
+    Time: ca. (136 + 41)min
 """
 
-from collections import defaultdict
-from typing import DefaultDict
+import re
 
 
-def data_input(filename: str = "data") -> str:
+def data_input(filename: str = "data") -> dict[str, list[tuple[str, int]]]:
     with open(filename) as file:
-        return file.read()
+        result: dict[str, list[tuple[str, int]]] = {}
+        for line in file.read().splitlines():
+            result |= line_to_dict(line)
+        return result
 
 
-def part_1():
-    pass
+def line_to_dict(line: str) -> dict[str, list[tuple[str, int]]]:
+    pattern_bag = re.compile(r"(\w+ \w+) bags contain")
+    bag = re.search(pattern_bag, line).group(1)
+
+    pattern_inside_bag = re.compile(r"(\d) (\w+ \w+) bag")
+    inside_bags = re.findall(pattern_inside_bag, line)
+    if inside_bags:
+        return {bag: [(inside_bag[1], int(inside_bag[0])) for inside_bag in inside_bags]}
+    else:
+        return {bag: [("", 0)]}
 
 
-def part_2():
-    pass
+class Bag:
+    bag_rules: dict[str, list[tuple[str, int]]] = {}
+
+    def __init__(self, name) -> None:
+        self.name = name
+        self.containing: set[str] = set()
+        self.not_containing: set[str] = set()
+        self.amount: int = 0
+
+    def contains_one(self, possible_bag) -> None:
+        for inside_bag in self.bag_rules[possible_bag]:
+            if not inside_bag[0]:
+                self.not_containing.add(possible_bag)
+                break
+            elif inside_bag[0] == self.name or inside_bag[0] in self.containing:
+                self.containing.add(possible_bag)
+                break
+            else:
+                self.contains_one(inside_bag[0])
+
+            if inside_bag[0] in self.containing:
+                self.containing.add(possible_bag)
+                break
+        else:
+            self.not_containing.add(possible_bag)
+
+    def contains(self) -> None:
+        for bag in self.bag_rules:
+            self.contains_one(bag)
+
+    def amount_containing(self, start_name: str, start_value: int = 1) -> None:
+        if start_name:
+            for inside_bag in self.bag_rules[start_name]:
+                if inside_bag[1]:
+                    self.amount += start_value * inside_bag[1]
+                    self.amount_containing(
+                        inside_bag[0], start_value * inside_bag[1])
+
+
+def part_1(bag: str = "shiny gold") -> int:
+    my_bag = Bag(bag)
+    my_bag.contains()
+    return len(my_bag.containing)
+
+
+def part_2(bag: str = "shiny gold") -> int:
+    my_bag = Bag(bag)
+    my_bag.amount_containing(my_bag.name)
+    return my_bag.amount
 
 
 def main() -> None:
-    data = data_input("test_data")
+    Bag.bag_rules = data_input("data")
 
     p1 = part_1()
-    print(f"Part 1: {p1} is {p1 == 0}")
+    print(f"Part 1: {p1} is {p1 == 179}")
 
     p2 = part_2()
-    print(f"Part 2: {p2} is {p2 == 0}")
+    print(f"Part 2: {p2} is {p2 == 18925}")
 
 
 if __name__ == "__main__":
     main()
+
+    # import timeit
+    # print(timeit.timeit("part_1()", globals=globals(), number=100))
+    # print(timeit.timeit("part_2()", globals=globals(), number=10_000))
--- 07/test.py
+++ 07/test.py
@@ -1,25 +1,66 @@
 """test_aoc_07"""
 
 import unittest
-from main import data_input, part_1, part_2
+from main import data_input, Bag, part_1, part_2
 
 
-class TestAoC07(unittest.TestCase):
+class TestAoC07_1(unittest.TestCase):
     """()"""
 
     @classmethod
     def setUpClass(self):
-        self.data = data_input("test_data")
+        Bag.bag_rules = data_input("test_data")
+        self.my_bag = Bag("shiny gold")
+
+    def test_contains_first_level_single_bag_inside(self):
+        possible_bag = "bright white"
+        self.my_bag.contains_one(possible_bag)
+        self.assertTrue(possible_bag in self.my_bag.containing)
+
+    def test_contains_first_level_multiple_bags_inside(self):
+        possible_bag = "muted yellow"
+        self.my_bag.contains_one(possible_bag)
+        self.assertTrue(possible_bag in self.my_bag.containing)
+
+    def test_contains_second_level_multiple_bags_inside_1(self):
+        possible_bag = "dark orange"
+        self.my_bag.contains()
+        self.assertTrue(possible_bag in self.my_bag.containing)
+
+    def test_contains_second_level_multiple_bags_inside_2(self):
+        possible_bag = "light red"
+        self.my_bag.contains()
+        self.assertTrue(possible_bag in self.my_bag.containing)
 
     def test_part_1(self):
         """()"""
-        result = part_1(self.data)
-        self.assertEqual(result, 514579)
+        result = part_1()
+        self.assertEqual(result, 4)
+
+
+class TestAoC07_2(unittest.TestCase):
+    """()"""
+
+    @classmethod
+    def setUpClass(self):
+        Bag.bag_rules = data_input("test_data_2")
+
+    def test_amount_one_level_single_bag_inside(self):
+        result = part_2("dark violet")
+        self.assertEqual(result, 0)
+
+    def test_amount_two_levels_single_bag_inside(self):
+        result = part_2("dark blue")
+        self.assertEqual(result, 2)
+
+    def test_amount_three_levels_single_bag_inside(self):
+        result = part_2("dark green")
+        self.assertEqual(result, 6)
 
     def test_part_2(self):
         """()"""
-        result = part_2(self.data)
-        self.assertEqual(result, 241861950)
+        result = part_2()
+        self.assertEqual(result, 126)
 
 
 if __name__ == "__main__":
