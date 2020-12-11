d9e42c26b19b7fbf73bfae7514d90c30b90cabf2
Dominik <Dominik.Schillo@googlemail.com>
2020-12-11 11:35:41 +0100

    Day11 completed

--- 11/main.py
+++ 11/main.py
@@ -1,31 +1,98 @@
 """
     aoc_11
     https://adventofcode.com/2020/day/11
-    Time: ca. ( + )min
+    Time: ca. (51 + 49)min
 """
 
+from __future__ import annotations
+from typing import NamedTuple
 
-def data_input(filename: str = "data") -> str:
+
+def data_input(filename: str = "data") -> list[list[str]]:
     with open(filename) as file:
-        return file.read()
+        return data_transformation(file.read())
 
 
-def part_1():
-    pass
+def data_transformation(string: str) -> list[list[str]]:
+    return [list(seat_row) for seat_row in string.splitlines()]
 
 
-def part_2():
-    pass
+class Rules(NamedTuple):
+    adjacent: bool
+    max_occ_seats_around: int
+
+
+class Seats:
+    directions: list[tuple[int, int]] = [
+        (i, j) for i in range(-1, 2) for j in range(-1, 2) if (i, j) != (0, 0)]
+
+    def __init__(self, seat_layout: list[list[str]], rules: Rules) -> None:
+        self.seat_layout = seat_layout
+        self.rules = rules
+
+    @property
+    def occupied_seats(self) -> int:
+        return sum(symbol == "#" for row in self.seat_layout for symbol in row)
+
+    def occupied_seats_around(self, position: tuple[int, int]) -> int:
+        sm: int = 0
+        for direction in self.directions:
+            new_position = position
+            while True:
+                new_position = (
+                    new_position[0] + direction[0], new_position[1] + direction[1])
+                if 0 <= new_position[0] < len(self.seat_layout) and 0 <= new_position[1] < len(self.seat_layout[0]):
+                    if self.seat_layout[new_position[0]][new_position[1]] == "#":
+                        sm += 1
+                        break
+                    if self.seat_layout[new_position[0]][new_position[1]] == "L":
+                        break
+                else:
+                    break
+                if self.rules.adjacent:
+                    break
+        return sm
+
+    def round(self) -> Seats:
+        new_seat_layout = [[] for _ in self.seat_layout]
+        for row_index, seat_row in enumerate(self.seat_layout):
+            for column_index, seat in enumerate(seat_row):
+                if seat == "L" and not self.occupied_seats_around((row_index, column_index)):
+                    new_seat_layout[row_index].append("#")
+                elif seat == "#" and self.occupied_seats_around((row_index, column_index)) >= self.rules.max_occ_seats_around:
+                    new_seat_layout[row_index].append("L")
+                else:
+                    new_seat_layout[row_index].append(
+                        self.seat_layout[row_index][column_index])
+        return Seats(new_seat_layout, self.rules)
+
+
+def constructor(seats: Seats) -> int:
+    while True:
+        new_seats = seats.round()
+        if new_seats.seat_layout == seats.seat_layout:
+            break
+        else:
+            seats = new_seats
+    return new_seats.occupied_seats
+
+
+def part_1(seat_layout: list[str]) -> int:
+    return constructor(Seats(seat_layout, Rules(True, 4)))
+
+
+def part_2(seat_layout):
+    return constructor(Seats(seat_layout, Rules(False, 5)))
 
 
 def main() -> None:
-    data = data_input("test_data")
+    seat_layout = data_input("data")
 
-    p1 = part_1()
-    print(f"Part 1: {p1} is {p1 == 0}")
+    p1 = part_1(seat_layout)
+    print(f"Part 1: {p1} is {p1 == 2483}")
 
-    p2 = part_2()
-    print(f"Part 2: {p2} is {p2 == 0}")
+    p2 = part_2(seat_layout)
+    print(f"Part 2: {p2} is {p2 == 2285}")
 
 
 if __name__ == "__main__":
--- 11/test.py
+++ 11/test.py
@@ -1,7 +1,7 @@
 """test_aoc_11"""
 
 import unittest
-from main import data_input, part_1, part_2
+from main import data_input, data_transformation, Seats, Rules, part_1, part_2
 
 
 class TestAoC11(unittest.TestCase):
@@ -10,16 +10,51 @@ class TestAoC11(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.data = data_input("test_data")
+        cls.seats_1 = Seats(cls.data, Rules(True, 4))
+        cls.seats_2 = Seats(cls.data, Rules(False, 5))
 
     def test_part_1(self):
         """()"""
         result = part_1(self.data)
-        self.assertEqual(result, 0)
+        self.assertEqual(result, 37)
 
-    def test_part_2(self):
-        """()"""
-        result = part_2(self.data)
-        self.assertEqual(result, 0)
+    def test_round_1(self):
+        result = self.seats_1.round().seat_layout
+        expected_result = data_transformation("""#.##.##.##
+#######.##
+#.#.#..#..
+####.##.##
+#.##.##.##
+#.#####.##
+..#.#.....
+##########
+#.######.#
+#.#####.##""")
+        self.assertEqual(result, expected_result)
+
+    def test_occupied_seats_around(self):
+        seats = self.seats_1.round()
+        result = seats.occupied_seats_around((1, 0))
+        self.assertEqual(result, 3)
+    
+#     def test_new_round_2(self):
+#         result = new_round(new_round(self.data))
+#         expected_result = data_transformation("""#.LL.LL.L#
+# #LLLLLL.LL
+# L.L.L..L..
+# LLLL.LL.LL
+# L.LL.LL.LL
+# L.LLLLL.LL
+# ..L.L.....
+# LLLLLLLLL#
+# #.LLLLLL.L
+# #.LLLLL.L#""")
+#         self.assertEqual(result, expected_result)
+
+    # def test_part_2(self):
+    #     """()"""
+    #     result = part_2(self.data)
+    #     self.assertEqual(result, 26)
 
 
 if __name__ == "__main__":
