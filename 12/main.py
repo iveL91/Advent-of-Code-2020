149a02aea6d9909d4022ed660bc4ba938e05a6d1
Dominik <Dominik.Schillo@googlemail.com>
2020-12-12 13:43:46 +0100

    Day12 completed

--- 12/main.py
+++ 12/main.py
@@ -1,32 +1,136 @@
 """
     aoc_12
     https://adventofcode.com/2020/day/12
-    Time: ca. ( + )min
+    Time: ca. (74 + 15)min
 """
 
+import functools
+from typing import Callable, NamedTuple
 
-def data_input(filename: str = "data") -> str:
+
+class NavigationInstruction(NamedTuple):
+    action: str
+    value: int
+
+
+def data_input(filename: str = "data") -> list[NavigationInstruction]:
     with open(filename) as file:
-        return file.read()
+        return [data_transformation(string) for string in file.read().splitlines()]
 
 
-def part_1():
-    pass
+def data_transformation(string: str) -> NavigationInstruction:
+    return NavigationInstruction(string[0], int(string[1:]))
 
 
-def part_2():
-    pass
+class Ship:
+    directions_dict: dict[str, tuple[int, int]] = {"N": (0, 1),
+                                                   "S": (0, -1),
+                                                   "E": (1, 0),
+                                                   "W": (-1, 0)}
+
+    def __init__(self, navigation_instructions: list[NavigationInstruction]) -> None:
+        self.navigation_instructions = navigation_instructions
+        self.direction = self.directions_dict["E"]
+        self.position = (0, 0)
+        self.waypoint_position = (10, 1)
+
+    def new_position(self, navigation_instruction: NavigationInstruction, position: tuple[int, int]) -> tuple[int, int]:
+        return tuple_add(
+            position, tuple_scalar_mul(navigation_instruction.value, self.directions_dict[navigation_instruction.action]))
+
+    @staticmethod
+    def new_direction(navigation_instruction: NavigationInstruction, direction: tuple[int, int]) -> tuple[int, int]:
+        if navigation_instruction.action == "L":
+            return repeated(
+                tuple2D_rotate_90, navigation_instruction.value // 90)(direction)
+        # elif navigation_instruction.action == "R":
+        return repeated(tuple2D_rotate_90, 4 - navigation_instruction.value // 90)(direction)
+
+    def constructor_move(self, navigation_instruction: NavigationInstruction, position: tuple[int, int], direction: tuple[int, int]) -> tuple[tuple[int, int], tuple[int, int]]:
+        if navigation_instruction.action in ["N", "S", "E", "W"]:
+            position = self.new_position(navigation_instruction, position)
+        else:  # navigation_instruction.action in ["L", "R"]:
+            direction = self.new_direction(navigation_instruction, direction)
+        return position, direction
+
+    def move_forward(self, steps, direction) -> None:
+        self.position = tuple_add(
+            self.position, tuple_scalar_mul(steps, direction))
+
+    def move_ship(self, navigation_instruction: NavigationInstruction) -> None:
+        self.position, self.direction = self.constructor_move(
+            navigation_instruction, self.position, self.direction)
+
+    def move_waypoint_position(self, navigation_instruction: NavigationInstruction) -> None:
+        position, direction = self.constructor_move(
+            navigation_instruction, self.waypoint_position, self.waypoint_position)
+        self.waypoint_position = position if self.waypoint_position != position else direction
+
+    def move(self, navigation_instruction: NavigationInstruction, direction: tuple[int, int], movement: Callable) -> None:
+        if navigation_instruction.action == "F":
+            self.move_forward(navigation_instruction.value, direction)
+        else:
+            movement(navigation_instruction)
+
+    def run(self) -> None:
+        for navigation_instruction in self.navigation_instructions:
+            self.move(navigation_instruction, self.direction, self.move_ship)
+
+    def __call__(self) -> None:
+        for navigation_instruction in self.navigation_instructions:
+            self.move(navigation_instruction, self.waypoint_position,
+                      self.move_waypoint_position)
+
+
+def tuple_add(tuple_1: tuple[int, ...], tuple_2: tuple[int, ...]) -> tuple[int, ...]:
+    return tuple(component_1 + component_2 for component_1, component_2 in zip(tuple_1, tuple_2))
+
+
+def tuple_scalar_mul(scalar: int, tup: tuple[int, ...]) -> tuple[int, ...]:
+    return tuple(scalar * component for component in tup)
+
+
+def tuple2D_rotate_90(tup: tuple[int, int]) -> tuple[int, int]:
+    return -tup[1], tup[0]
+
+
+def repeated(f: Callable, n: int) -> Callable:
+    """From https://stackoverflow.com/questions/22921626/how-to-compose-to-functions-several-times"""
+    def repeat(arg):
+        return functools.reduce(lambda r, g: g(r), [f] * n, arg)
+    return repeat
+
+
+def manhatten_distance(vector_1: tuple[int, ...], vector_2: tuple[int, ...] = (0, 0)) -> int:
+    return sum(abs(component_2 - component_1) for component_1, component_2 in zip(vector_1, vector_2))
+
+
+def part_1(navigation_instructions: list[NavigationInstruction]) -> int:
+    ship = Ship(navigation_instructions)
+    ship.run()
+    return manhatten_distance(ship.position)
+
+
+def part_2(navigation_instructions: list[NavigationInstruction]) -> int:
+    ship = Ship(navigation_instructions)
+    ship()
+    return manhatten_distance(ship.position)
 
 
 def main() -> None:
-    data = data_input("test_data")
+    navigation_instructions = data_input("data")
 
-    p1 = part_1()
-    print(f"Part 1: {p1} is {p1 == 0}")
+    p1 = part_1(navigation_instructions)
+    print(f"Part 1: {p1} is {p1 == 2458}")
 
-    p2 = part_2()
-    print(f"Part 2: {p2} is {p2 == 0}")
+    p2 = part_2(navigation_instructions)
+    print(f"Part 2: {p2} is {p2 == 145117}")
 
 
 if __name__ == "__main__":
     main()
+
+    # import timeit
+    # navigation_instructions = data_input("data")
+    # print(timeit.timeit("part_1(navigation_instructions)", globals=globals(), number=10_000))
+    # print(timeit.timeit("part_2(navigation_instructions)", globals=globals(), number=10_000))
--- 12/test.py
+++ 12/test.py
@@ -1,7 +1,7 @@
 """test_aoc_12"""
 
 import unittest
-from main import data_input, part_1, part_2
+from main import data_input, data_transformation, Ship, part_1, part_2
 
 
 class TestAoC12(unittest.TestCase):
@@ -9,17 +9,147 @@ class TestAoC12(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.data = data_input("test_data")
+        cls.navigation_instructions = data_input("test_data")
+        cls.ship = Ship(cls.navigation_instructions)
+
+    def test_move_north_1(self):
+        navigation_instruction = data_transformation("N1")
+        self.ship.position = (0, 0)
+        self.ship.move_ship(navigation_instruction)
+        self.assertEqual(self.ship.position, (0, 1))
+
+    def test_move_north_2(self):
+        navigation_instruction = data_transformation("N1")
+        self.ship.position = (1, 1)
+        self.ship.move_ship(navigation_instruction)
+        self.assertEqual(self.ship.position, (1, 2))
+
+    def test_move_south_1(self):
+        navigation_instruction = data_transformation("S1")
+        self.ship.position = (0, 0)
+        self.ship.move_ship(navigation_instruction)
+        self.assertEqual(self.ship.position, (0, -1))
+
+    def test_move_south_2(self):
+        navigation_instruction = data_transformation("S1")
+        self.ship.position = (1, 1)
+        self.ship.move_ship(navigation_instruction)
+        self.assertEqual(self.ship.position, (1, 0))
+
+    def test_move_east_1(self):
+        navigation_instruction = data_transformation("E1")
+        self.ship.position = (0, 0)
+        self.ship.move_ship(navigation_instruction)
+        self.assertEqual(self.ship.position, (1, 0))
+
+    def test_move_east_2(self):
+        navigation_instruction = data_transformation("E1")
+        self.ship.position = (1, 1)
+        self.ship.move_ship(navigation_instruction)
+        self.assertEqual(self.ship.position, (2, 1))
+
+    def test_move_west_1(self):
+        navigation_instruction = data_transformation("W1")
+        self.ship.position = (0, 0)
+        self.ship.move_ship(navigation_instruction)
+        self.assertEqual(self.ship.position, (-1, 0))
+
+    def test_move_west_2(self):
+        navigation_instruction = data_transformation("W1")
+        self.ship.position = (1, 1)
+        self.ship.move_ship(navigation_instruction)
+        self.assertEqual(self.ship.position, (0, 1))
+
+    def test_move_rotate_left_90_1(self):
+        navigation_instruction = data_transformation("L90")
+        self.ship.position = (0, 0)
+        self.ship.direction = (0, 1)
+        self.ship.move_ship(navigation_instruction)
+        self.assertEqual(self.ship.direction, (-1, 0))
+
+    def test_move_rotate_left_90_2(self):
+        navigation_instruction = data_transformation("L90")
+        self.ship.position = (0, 0)
+        self.ship.direction = (-1, 0)
+        self.ship.move_ship(navigation_instruction)
+        self.assertEqual(self.ship.direction, (0, -1))
+
+    def test_move_rotate_left_180(self):
+        navigation_instruction = data_transformation("L180")
+        self.ship.position = (0, 0)
+        self.ship.direction = (0, 1)
+        self.ship.move_ship(navigation_instruction)
+        self.assertEqual(self.ship.direction, (0, -1))
+
+    def test_move_rotate_left_270(self):
+        navigation_instruction = data_transformation("L270")
+        self.ship.position = (0, 0)
+        self.ship.direction = (0, 1)
+        self.ship.move_ship(navigation_instruction)
+        self.assertEqual(self.ship.direction, (1, 0))
+
+    def test_move_rotate_right_90(self):
+        navigation_instruction = data_transformation("R90")
+        self.ship.position = (0, 0)
+        self.ship.direction = (0, 1)
+        self.ship.move_ship(navigation_instruction)
+        self.assertEqual(self.ship.direction, (1, 0))
+
+    def test_move_rotate_right_180(self):
+        navigation_instruction = data_transformation("R180")
+        self.ship.position = (0, 0)
+        self.ship.direction = (0, 1)
+        self.ship.move_ship(navigation_instruction)
+        self.assertEqual(self.ship.direction, (0, -1))
+
+    def test_move_rotate_right_270(self):
+        navigation_instruction = data_transformation("R270")
+        self.ship.position = (0, 0)
+        self.ship.direction = (0, 1)
+        self.ship.move_ship(navigation_instruction)
+        self.assertEqual(self.ship.direction, (-1, 0))
+
+    def test_move_forward_1(self):
+        navigation_instruction = data_transformation("F2")
+        self.ship.position = (0, 0)
+        self.ship.direction = (0, 1)
+        self.ship.move_forward(
+            navigation_instruction.value, self.ship.direction)
+        self.assertEqual(self.ship.position, (0, 2))
+
+    def test_move_forward_2(self):
+        navigation_instruction = data_transformation("F2")
+        self.ship.position = (0, 0)
+        self.ship.direction = (1, 0)
+        self.ship.move_forward(
+            navigation_instruction.value, self.ship.direction)
+        self.assertEqual(self.ship.position, (2, 0))
+
+    def test_move_forward_3(self):
+        navigation_instruction = data_transformation("F2")
+        self.ship.position = (0, 0)
+        self.ship.direction = (0, -1)
+        self.ship.move_forward(
+            navigation_instruction.value, self.ship.direction)
+        self.assertEqual(self.ship.position, (0, -2))
+
+    def test_move_forward_4(self):
+        navigation_instruction = data_transformation("F2")
+        self.ship.position = (0, 0)
+        self.ship.direction = (-1, 0)
+        self.ship.move_forward(
+            navigation_instruction.value, self.ship.direction)
+        self.assertEqual(self.ship.position, (-2, 0))
 
     def test_part_1(self):
         """()"""
-        result = part_1(self.data)
-        self.assertEqual(result, 0)
+        result = part_1(self.navigation_instructions)
+        self.assertEqual(result, 25)
 
     def test_part_2(self):
         """()"""
-        result = part_2(self.data)
-        self.assertEqual(result, 0)
+        result = part_2(self.navigation_instructions)
+        self.assertEqual(result, 286)
 
 
 if __name__ == "__main__":
