285c84e8e2ccd1e5be0d03e4b090adfca5f78064
Dominik <Dominik.Schillo@googlemail.com>
2020-12-16 16:26:47 +0100

    Day16 improved, tests added

--- 16/main.py
+++ 16/main.py
@@ -7,60 +7,63 @@
 import re
 from functools import reduce
 from operator import mul
-from typing import Sequence
+from typing import Iterable, NamedTuple
 
 
-def data_input(filename: str = "data") -> dict[str, list]:
+class Data(NamedTuple):
+    properties: dict[str, list[tuple[int, int]]]
+    your_ticket: list[int]
+    nearby_tickets: list[list[int]]
+
+
+def data_input(filename: str = "data") -> Data:
     with open(filename) as file:
         return data_transformation(file.read())
 
 
-def data_transformation(string: str) -> dict[str, list]:
+def data_transformation(string: str) -> Data:
     lst = [split.split("\n") for split in string.split("\n\n")]
-    dct = {}
 
-    property_dict = {}
+    property_dict: dict[str, list[tuple[int, int]]] = {}
     pattern = re.compile(r"(.+): (\d+)-(\d+) or (\d+)-(\d+)")
-    for property in lst[0]:
-        matches = re.search(pattern, property)
-        property_dict[matches.group(1)] = [(int(matches.group(2)), int(matches.group(3))),
-                                           (int(matches.group(4)), int(matches.group(5)))]
+    for prop in lst[0]:
+        if matches := re.search(pattern, prop):
+            property_dict[matches.group(1)] = [(int(matches.group(2)), int(matches.group(3))),
+                                               (int(matches.group(4)), int(matches.group(5)))]
 
-    dct["properties"] = property_dict
-    dct[lst[1][0][:-1]] = [int(number) for number in lst[1][1].split(",")]
-    dct[lst[2][0][:-1]] = [[int(number) for number in ticket.split(",")]
-                           for ticket in lst[2][1:]]
-    return dct
+    return Data(properties=property_dict,
+                your_ticket=[int(number) for number in lst[1][1].split(",")],
+                nearby_tickets=[[int(number) for number in ticket.split(",")] for ticket in lst[2][1:]])
 
 
 def in_range(number: int, lower: int, upper: int) -> bool:
     return lower <= number <= upper
 
 
-def valid_number(number: int, property_ranges: Sequence[tuple[int, int]]) -> bool:
-    for valid_ranges in property_ranges:
-        for valid_range in valid_ranges:
-            if in_range(number, *valid_range):
+def valid_number(number: int, properties_ranges: Iterable[Iterable[tuple[int, int]]]) -> bool:
+    for property_ranges in properties_ranges:
+        for property_range in property_ranges:
+            if in_range(number, *property_range):
                 return True
     return False
 
 
-def ticket_scanning_error_rate(nearby_tickets: list[list[int]], property_dict: dict[str, list[tuple[int, int]]]) -> int:
+def ticket_scanning_error_rate(nearby_tickets: Iterable[Iterable[int]], property_dict: dict[str, list[tuple[int, int]]]) -> int:
     return sum(number for ticket in nearby_tickets for number in ticket if not valid_number(number, property_dict.values()))
 
 
-def part_1(data: dict[str, list]) -> int:
-    return ticket_scanning_error_rate(data["nearby tickets"], data["properties"])
+def part_1(data: Data) -> int:
+    return ticket_scanning_error_rate(data.nearby_tickets, data.properties)
 
 
-def valid_ticket(ticket: Sequence[int], property_ranges: Sequence[tuple[int, int]]) -> bool:
+def valid_ticket(ticket: Iterable[int], properties_ranges: Iterable[Iterable[tuple[int, int]]]) -> bool:
     for number in ticket:
-        if not valid_number(number, property_ranges):
+        if not valid_number(number, properties_ranges):
             return False
     return True
 
 
-def valid_property(property_ranges: Sequence[tuple[int, int]], values: Sequence[int]) -> bool:
+def valid_property(property_ranges: Iterable[tuple[int, int]], values: Iterable[int]) -> bool:
     for value in values:
         for property_range in property_ranges:
             if in_range(value, *property_range):
@@ -70,12 +73,12 @@ def valid_property(property_ranges: Sequence[tuple[int, int]], values: Sequence[
     return True
 
 
-def possible_properties(value_list: list[int], property_dict: dict[str, list[tuple[int, int]]]) -> list[str]:
-    return [property for property, property_ranges in property_dict.items() if valid_property(property_ranges, value_list)]
+def possible_properties(values: Iterable[int], property_dict: dict[str, list[tuple[int, int]]]) -> list[str]:
+    return [prop for prop, property_ranges in property_dict.items() if valid_property(property_ranges, values)]
 
 
 def determine_unique_property(column_dict: dict[int, list[str]]) -> dict[str, int]:
-    properties_taken = set()
+    properties_taken: set[str] = set()
     amount_columns = len(column_dict)
     while len(properties_taken) < amount_columns:
         for column_index, properties in column_dict.items():
@@ -89,25 +92,27 @@ def determine_unique_property(column_dict: dict[int, list[str]]) -> dict[str, in
     return {prop[0]: index for index, prop in column_dict.items()}
 
 
-def prod(numbers: Sequence[int]) -> int:
-    return reduce(mul, numbers, 1)
+def fields(data: Data) -> dict[str, int]:
+    valid_tickets = [ticket for ticket in data.nearby_tickets
+                     if valid_ticket(ticket, data.properties.values())]
 
-
-def part_2(data: dict[str, list]) -> int:
-    valid_tickets = [ticket for ticket in data["nearby tickets"]
-                     if valid_ticket(ticket, data["properties"].values())]
-
-    amount_propoerties = len(data["your ticket"])
+    amount_propoerties = len(data.your_ticket)
     # column_dict = {column_index: possible_properties(
     #     [ticket[column_index] for ticket in valid_tickets], data["properties"]) for column_index in range(amount_propoerties)}
     column_dict = {}
     for column_index in range(amount_propoerties):
         column = [ticket[column_index] for ticket in valid_tickets]
         column_dict[column_index] = possible_properties(
-            column, data["properties"])
+            column, data.properties)
 
-    prop_index_dict = determine_unique_property(column_dict)
-    return prod(data["your ticket"][value] for key, value in prop_index_dict.items() if key.startswith("departure"))
+    return determine_unique_property(column_dict)
+
+def prod(numbers: Iterable[int]) -> int:
+    return reduce(mul, numbers, 1)
+
+
+def part_2(data: Data) -> int:
+    return prod(data.your_ticket[value] for key, value in fields(data).items() if key.startswith("departure"))
 
 
 def main() -> None:
--- 16/test.py
+++ 16/test.py
@@ -1,7 +1,7 @@
 """test_aoc_16"""
 
 import unittest
-from main import data_input, part_1, part_2
+from main import data_input, valid_number, part_1, valid_ticket, fields, part_2
 
 
 class TestAoC16(unittest.TestCase):
@@ -9,17 +9,90 @@ class TestAoC16(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.data = data_input("test_data_1")
+        cls.data = data_input("data")
+        cls.data_1 = data_input("test_data_1")
+        cls.data_2 = data_input("test_data_2")
 
-    def test_part_1(self):
+    def test_valid_number_true_1(self):
+        result = valid_number(
+            self.data_1.nearby_tickets[0][0], self.data_1.properties.values())
+        self.assertTrue(result)
+
+    def test_valid_number_true_2(self):
+        result = valid_number(
+            self.data_1.nearby_tickets[0][1], self.data_1.properties.values())
+        self.assertTrue(result)
+
+    def test_valid_number_true_3(self):
+        result = valid_number(
+            self.data_1.nearby_tickets[0][2], self.data_1.properties.values())
+        self.assertTrue(result)
+
+    def test_valid_number_false_1(self):
+        result = valid_number(
+            self.data_1.nearby_tickets[1][1], self.data_1.properties.values())
+        self.assertFalse(result)
+
+    def test_valid_number_false_2(self):
+        result = valid_number(
+            self.data_1.nearby_tickets[2][0], self.data_1.properties.values())
+        self.assertFalse(result)
+
+    def test_valid_number_false_3(self):
+        result = valid_number(
+            self.data_1.nearby_tickets[3][2], self.data_1.properties.values())
+        self.assertFalse(result)
+
+    def test_part_1_1(self):
+        """()"""
+        result = part_1(self.data_1)
+        self.assertEqual(result, 71)
+
+    def test_part_1_2(self):
         """()"""
         result = part_1(self.data)
-        self.assertEqual(result, 0)
+        self.assertEqual(result, 20060)
+
+    def test_valid_ticket(self):
+        result = valid_ticket(
+            self.data_1.nearby_tickets[0], self.data_1.properties.values())
+        self.assertTrue(result)
+
+    def test_fields_1(self):
+        result = fields(self.data_2)
+        expected_result = {"row": 0,
+                           "class": 1,
+                           "seat": 2}
+        self.assertEqual(result, expected_result)
+
+    def test_fields_2(self):
+        result = fields(self.data)
+        expected_result = {'arrival platform': 0,
+                           'departure station': 1,
+                           'departure track': 2,
+                           'type': 3,
+                           'class': 4,
+                           'departure location': 5,
+                           'arrival track': 6,
+                           'duration': 7,
+                           'departure platform': 8,
+                           'zone': 9,
+                           'row': 10,
+                           'departure time': 11,
+                           'route': 12,
+                           'wagon': 13,
+                           'seat': 14,
+                           'departure date': 15,
+                           'arrival station': 16,
+                           'arrival location': 17,
+                           'price': 18,
+                           'train': 19}
+        self.assertEqual(result, expected_result)
 
     def test_part_2(self):
         """()"""
         result = part_2(self.data)
-        self.assertEqual(result, 0)
+        self.assertEqual(result, 2843534243843)
 
 
 if __name__ == "__main__":
