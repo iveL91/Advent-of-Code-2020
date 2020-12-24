16abfcbf777cf68c35abc3df18ff45adc3805f4e
Dominik <Dominik.Schillo@googlemail.com>
2020-12-24 17:57:37 +0100

    Day24 complete

--- 24/main.py
+++ 24/main.py
@@ -1,32 +1,113 @@
 """
     aoc_24
     https://adventofcode.com/2020/day/24
-    Time: ca. ( + )min
+    Time: ca. (47 + 97)min
 """
 
 
-def data_input(filename: str) -> str:
+from typing import DefaultDict
+
+Instruction = list[tuple[int, int]]
+
+DIRECTIONS_DICT: dict[str, tuple[int, int]] = {
+    "e": (0, 2),
+    "se": (-1, 1),
+    "sw": (-1, -1),
+    "w": (0, -2),
+    "nw": (1, -1),
+    "ne": (1, 1)
+}
+
+
+def data_input(filename: str) -> list[Instruction]:
     with open(filename) as file:
-        return file.read()
+        return [data_transformation(line) for line in file.read().splitlines()]
 
 
-def part_1(data):
-    pass
+def data_transformation(line: str) -> Instruction:
+    instruction: Instruction = []
+    while line:
+        if line[0] in ["n", "s"]:
+            instruction.append(DIRECTIONS_DICT[line[:2]])
+            line = line[2:]
+        else:
+            instruction.append(DIRECTIONS_DICT[line[0]])
+            line = line[1:]
+    return instruction
 
 
-def part_2(data):
-    pass
+def tuple_add(tuple_1: tuple[int, ...], tuple_2: tuple[int, ...]) -> tuple[int, ...]:
+    return tuple(component_1 + component_2 for component_1, component_2 in zip(tuple_1, tuple_2))
+
+
+def constructor(instructions: list[Instruction]) -> dict[tuple[int, int], bool]:
+    tiles: DefaultDict[tuple[int, int], bool] = DefaultDict(bool)
+    for instruction in instructions:
+        tile_direction = (0, 0)
+        for direction in instruction:
+            tile_direction = tuple_add(tile_direction, direction)
+        tiles[tile_direction] = not tiles[tile_direction]
+    return tiles
+
+
+def count_black_tiles(tiles: dict[tuple[int, int], bool]) -> int:
+    return sum(tiles.values())
+
+
+def part_1(instructions: list[Instruction]) -> int:
+    tiles = constructor(instructions)
+    return count_black_tiles(tiles)
+
+
+def determine_amount_neighbors(tile_position: tuple[int, int], extended_tiles: dict[tuple[int, int], bool]) -> int:
+    neighbor_values = [extended_tiles[tuple_add(
+        tile_position, direction)] for direction in DIRECTIONS_DICT.values()]
+    return sum(neighbor_values)
+
+
+def extend_grid(tiles: dict[tuple[int, int], bool]) -> dict[tuple[int, int], bool]:
+    additional_tiles = {tuple_add(
+        tile_position, direction): False for tile_position in tiles for direction in DIRECTIONS_DICT.values()}
+
+    return additional_tiles | tiles
+
+
+def flipping(tiles: dict[tuple[int, int], bool]) -> dict[tuple[int, int], bool]:
+    new_tiles = extend_grid(tiles)
+    extended_tiles = extend_grid(new_tiles)
+
+    neighbor_dict = {tile_position: determine_amount_neighbors(
+        tile_position, extended_tiles) for tile_position in new_tiles}
+
+    for tile_position, tile_value in new_tiles.items():
+        if tile_value and (not neighbor_dict[tile_position] or neighbor_dict[tile_position] > 2):
+            new_tiles[tile_position] = not new_tiles[tile_position]
+        elif not tile_value and neighbor_dict[tile_position] == 2:
+            new_tiles[tile_position] = not new_tiles[tile_position]
+    return new_tiles
+
+
+def part_2(instructions: list[Instruction], days: int = 100) -> int:
+    tiles = constructor(instructions)
+    for _ in range(days):
+        tiles = flipping(tiles)
+    return count_black_tiles(tiles)
 
 
 def main() -> None:
-    data = data_input("test_data")
+    instructions = data_input("data")
 
-    p1 = part_1(data)
-    print(f"Part 1: {p1} is {p1 == 0}")
+    p1 = part_1(instructions)
+    print(f"Part 1: {p1} is {p1 == 373}")
 
-    p2 = part_2(data)
-    print(f"Part 2: {p2} is {p2 == 0}")
+    p2 = part_2(instructions)
+    print(f"Part 2: {p2} is {p2 == 3917}")
 
 
 if __name__ == "__main__":
     main()
+
+    # import timeit
+    # INSTRUCTIONS = data_input("data")
+    # print(timeit.timeit("part_1(INSTRUCTIONS)", globals=globals(), number=1_000))
+    # print(timeit.timeit("part_2(INSTRUCTIONS)", globals=globals(), number=1))
--- 24/test.py
+++ 24/test.py
@@ -12,15 +12,93 @@ class TestAoC24(unittest.TestCase):
         cls.test_data = data_input("test_data")
         cls.data = data_input("data")
 
-    def test_part_1(self):
-        """()"""
-        result = part_1(self.data)
-        self.assertEqual(result, 0)
+    def test_part_1_1(self):
+        result = part_1(self.test_data)
+        self.assertEqual(result, 10)
 
-    def test_part_2(self):
-        """()"""
+    def test_part_1_2(self):
+        result = part_1(self.data)
+        self.assertEqual(result, 373)
+
+    def test_part_2_1(self):
+        result = part_2(self.test_data, 1)
+        self.assertEqual(result, 15)
+
+    def test_part_2_2(self):
+        result = part_2(self.test_data, 2)
+        self.assertEqual(result, 12)
+
+    def test_part_2_3(self):
+        result = part_2(self.test_data, 3)
+        self.assertEqual(result, 25)
+
+    def test_part_2_4(self):
+        result = part_2(self.test_data, 4)
+        self.assertEqual(result, 14)
+
+    def test_part_2_5(self):
+        result = part_2(self.test_data, 5)
+        self.assertEqual(result, 23)
+
+    def test_part_2_6(self):
+        result = part_2(self.test_data, 6)
+        self.assertEqual(result, 28)
+
+    def test_part_2_7(self):
+        result = part_2(self.test_data, 7)
+        self.assertEqual(result, 41)
+
+    def test_part_2_8(self):
+        result = part_2(self.test_data, 8)
+        self.assertEqual(result, 37)
+
+    def test_part_2_9(self):
+        result = part_2(self.test_data, 9)
+        self.assertEqual(result, 49)
+
+    def test_part_2_10(self):
+        result = part_2(self.test_data, 10)
+        self.assertEqual(result, 37)
+
+    def test_part_2_11(self):
+        result = part_2(self.test_data, 20)
+        self.assertEqual(result, 132)
+
+    def test_part_2_12(self):
+        result = part_2(self.test_data, 30)
+        self.assertEqual(result, 259)
+
+    def test_part_2_13(self):
+        result = part_2(self.test_data, 40)
+        self.assertEqual(result, 406)
+
+    def test_part_2_14(self):
+        result = part_2(self.test_data, 50)
+        self.assertEqual(result, 566)
+
+    def test_part_2_15(self):
+        result = part_2(self.test_data, 60)
+        self.assertEqual(result, 788)
+
+    def test_part_2_16(self):
+        result = part_2(self.test_data, 70)
+        self.assertEqual(result, 1106)
+
+    def test_part_2_17(self):
+        result = part_2(self.test_data, 80)
+        self.assertEqual(result, 1373)
+
+    def test_part_2_18(self):
+        result = part_2(self.test_data, 90)
+        self.assertEqual(result, 1844)
+
+    def test_part_2_19(self):
+        result = part_2(self.test_data)
+        self.assertEqual(result, 2208)
+
+    def test_part_2_20(self):
         result = part_2(self.data)
-        self.assertEqual(result, 0)
+        self.assertEqual(result, 3917)
 
 
 if __name__ == "__main__":
